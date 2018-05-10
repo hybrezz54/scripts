ed078e61176869ae323edb34bc6fcb940661165f
Hamzah Aslam <haz540@gmail.com>
2018-05-10 19:29:03 -0400

    Added update messages

--- new_windows_setup.ps1
+++ new_windows_setup.ps1
@@ -52,7 +52,7 @@ choco install discord
 Write-Host "Done`n" -ForegroundColor Green
 
 # Check if Spotify is installed
-Write-Host "Checking if Spotify and Slack are installed..."
+Write-Host "Checking if Spotify is installed..."
 $spotify = $false
 Get-AppxPackage -AllUsers | Foreach {
     If ($_.Name -eq 'SpotifyAB.SpotifyMusic') {
@@ -62,6 +62,7 @@ Get-AppxPackage -AllUsers | Foreach {
 }
 
 # Check if Slack is installed
+Write-Host "Checking if Slack is installed..."
 $slack = $false
 Get-AppxPackage -AllUsers | Foreach {
     If ($_.Name -eq '91750D7E.Slack') {
@@ -72,9 +73,9 @@ Get-AppxPackage -AllUsers | Foreach {
 
 # Open windows store otherwise
 If ($spotify -eq $false -Or $slack -eq $false) {
-    If ($spotify -eq $false -And $slack -eq $false) { Write-Host "Install Slack and Spotify from the Windows Store!" }
-    ElseIf ($spotify -eq $false) { Write-Host "Install Spotify from the Windows Store!" }
-    ElseIf ($slack -eq $false) { Write-Host "Install Slack from the Windows Store!" }
+    If ($spotify -eq $false -And $slack -eq $false) { Write-Host "Install Slack and Spotify from the Windows Store!" -ForegroundColor Red }
+    ElseIf ($spotify -eq $false) { Write-Host "Install Spotify from the Windows Store!" -ForegroundColor Red }
+    ElseIf ($slack -eq $false) { Write-Host "Install Slack from the Windows Store!`n" -ForegroundColor Red }
     explorer.exe shell:AppsFolder\Microsoft.WindowsStore_8wekyb3d8bbwe!App
 }
 
@@ -83,8 +84,12 @@ $dir = [Environment]::GetFolderPath('MyDocuments')
 Set-Location -Path $dir
 
 # Clone the scripts repo from Github
+Write-Host "Cloning scripts repository..."
 git clone https://github.com/hybrezz54/scripts.git
+Write-Host "Done`n" -ForegroundColor Green
 
 # Add scripts directory to user's PATH
+Write-Host "Adding it to the user's PATH environment variable..."
 $path = [Environment]::GetEnvironmentVariable("Path", "User")
 [Environment]::SetEnvironmentVariable("Path", $path + $dir + "\scripts", "User")
+Write-Host "Done`n" -ForegroundColor Green
