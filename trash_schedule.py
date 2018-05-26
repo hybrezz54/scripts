fa043e4b86c8ba253f004f1a2bc7ecc0e0d6bad4
Hamzah Aslam <haz540@gmail.com>
2018-05-26 19:22:54 -0400

    Cleanup

--- trash_schedule.py
+++ trash_schedule.py
@@ -8,15 +8,7 @@ in the trash collection schedule due to holidays.
 import re
 from datetime import datetime
 import requests
-import io
 from bs4 import BeautifulSoup
-import PyPDF2
-
-# URL for main site of schedule
-BASE_URL = "http://www.townofcary.org"
-
-# URL for the recycling schedule
-RECYCLING_URL = "http://www.townofcary.org/services-publications/garbage-recycling-yard-waste/recycling"
 
 # URL for the holiday schedule
 HOLIDAY_URL = "http://www.townofcary.org/services-publications/garbage-recycling-yard-waste/garbage-collection/holiday-schedule"
@@ -37,13 +29,8 @@ def process_recycling():
     """Obtains the results for the correct recycling schedule."""
 
     # download html for recycling schedule
-    print(f'Downloading page {RECYCLING_URL}...')
-    resource = requests.get(RECYCLING_URL)
-    resource.raise_for_status()
-
-    # parse html
-    soup = BeautifulSoup(resource.text, 'html.parser')
-
+    print('Downloading page ...')
+    
     # get correct schedule from user
     week = input("Type 'b' for Blue Week and 'y' for Yellow Week: ")[0].lower()
 
@@ -54,18 +41,6 @@ def process_recycling():
         week = 'Yellow'
     else:
         raise ValueError("Input must be 'b' or 'y'!")
-
-    # iterate over matched links
-    for link in soup.find_all('a', text=re.compile(f'({week})+')):
-        # check if contains appropriate year
-        if str(today.year) in link.text:
-            # process pdf
-            resource = requests.get(BASE_URL + link.get('href'))
-            stream = io.BytesIO(resource.content)
-            pdf = PyPDF2.PdfFileReader(stream)
-            # TODO parse PDF
-            print(pdf.getPage(0).extractText())
-            
 
 def process_holiday():
     """Obtains the results for the holiday schedule."""
